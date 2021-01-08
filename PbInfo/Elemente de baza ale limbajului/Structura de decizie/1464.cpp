0d7651657ee3a5e6914a01bd97cc09341ae122e1
CVOvidiu <cvovidiu2002@gmail.com>
2021-01-08 07:54:06 -0800

    08/01/21 : 1464.cpp

--- PbInfo/Elemente de baza ale limbajului/Structura de decizie/1464.cpp
+++ PbInfo/Elemente de baza ale limbajului/Structura de decizie/1464.cpp
@@ -3,5 +3,25 @@
 using namespace std;
 
 int main() {
-    
+    int Start, Cautat;
+    cin >> Start >> Cautat;
+
+    /*
+    int Element = 1;
+    while(c != Cautat) {
+        if(c % 2 == 0)
+            Element += 2;
+        else
+            Element += 1;
+
+        c++;
+    }
+    */
+
+    if(Cautat % 2 == 0) {
+        
+    }
+    else {
+
+    }
 }
