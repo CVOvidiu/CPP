70f8ce508db60b386dd6a19d526dcb58edc084b1
CVOvidiu <cvovidiu2002@gmail.com>
2021-01-09 18:27:54 -0800

    Switch PC

--- PbInfo/Elemente de baza ale limbajului/Structura de decizie/2616.cpp
+++ PbInfo/Elemente de baza ale limbajului/Structura de decizie/2616.cpp
@@ -5,5 +5,28 @@
 using namespace std;
 
 int main() {
-   
+   int linii, coloane, P, Start;
+   cin >> linii >> coloane;
+
+    if(linii % 2 == 0 && linii == coloane) {
+        P = (linii / 2) * (linii / 2);
+    }
+    else if(linii % 2 != 0 && linii == coloane) {
+        linii++;
+        coloane++;
+
+        P = (linii / 2) * (linii / 2);
+    }
+
+    if(linii % 2 == 0 && coloane % 2 == 0) {
+        Start = linii / 2;
+
+        P = Start * (linii / 2)
+    }
+    else if(linii % 2 != 0) {
+        linii++;
+        Start = linii / 2;
+    }
+
+    cout << P;
 }
